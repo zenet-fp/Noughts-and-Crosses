84c28277d78848a7d224a2eaad0036a7a53a82a5
zenet-fp <shaf2446@gmail.com>
2024-12-12 18:01:01 +0000

    Update nought_cross.py

--- nought_cross.py
+++ nought_cross.py
@@ -1,7 +1,5 @@
 import random
 
-
-
 class NoughtsCrosses:
     def __init__(self):
         self.score = []
@@ -14,34 +12,28 @@ class NoughtsCrosses:
             [" ", " ", " "],
             [" ", " ", " "]
         ]
-        self.positions = {"TL" : [0, 0], "TM" : [0, 1],
-                     "TR" : [0, 2], "ML" : [1, 0],
-                     "MM" : [1, 1], "MR" : [1, 2],
-                     "BL" : [2, 0], "BM" : [2, 1],
-                     "BR" : [2, 2]
-        }
-
-
+        self.positions = {"TL": [0, 0], "TM": [0, 1],
+                          "TR": [0, 2], "ML": [1, 0],
+                          "MM": [1, 1], "MR": [1, 2],
+                          "BL": [2, 0], "BM": [2, 1],
+                          "BR": [2, 2]
+                          }
 
     def main_game(self):
 
-        winner = DecideWinner()
         while self.user_decide is False and self.ai_decide is False:
 
             self.user_choice()
-            winner.choose_winner()
+            self.choose_winner()
             if self.user_decide is True or self.ai_decide is True:
-
                 break
-
+                
             self.ai_choice()
-            winner.choose_winner()
-            if self.user_decide is True or self.ai_decide is True :
-
+            self.choose_winner()
+            if self.user_decide is True or self.ai_decide is True:
                 break
 
             self.game_board()
-
 
     def game_board(self):
         for row in self.board:
@@ -59,11 +51,10 @@ class NoughtsCrosses:
 
         while self.player_piece is None:
             self.game_board()
-            #print("<-------------------------------------------------------------------------------->")
+            # print("<-------------------------------------------------------------------------------->")
             user_piece = str((input("Which piece are you using: Noughts or Crosses? (O / X) : "))).upper()
             self.player_piece = user_piece
             break
-
 
         l = True
         while l:
@@ -86,8 +77,13 @@ class NoughtsCrosses:
                     print("A piece is already in that position.")
 
             else:
-                print("An error occurred")
-                self.user_choice()
+
+                if user_position not in self.positions:
+                    print("A piece is already in that position. ")
+
+                else:
+                    print("An error occurred")
+
 
     def ai_choice(self):
 
@@ -102,30 +98,13 @@ class NoughtsCrosses:
             self.ai_piece = ai_piece
             self.board[ai_row][ai_column] = f"{ai_piece}"
 
-
-class DecideWinner(NoughtsCrosses):
-    def __init__(self):
-        super().__init__()
-
     def choose_winner(self):
-            x = self.player_piece
-            for i in range(3):
+        x = self.player_piece
+        for i in range(3):
 
-                if self.board[0][i] ==  self.board[1][i] == self.board[2][i] != " ":
+            if self.board[0][i] == self.board[1][i] == self.board[2][i] != " ":
 
-                    if self.board[0][i] == x:
-                        self.user_decide =  True
-                        self.game_board()
-                        print(f"The player has won three in a row!")
-
-                    else:
-                        self.ai_decide = True
-                        self.game_board()
-                        print(f"The AI has beaten the player! ")
-
-            if self.board[0][0] == self.board[1][1] == self.board[2][2] != " ":
-
-                if self.board[0][0] == x:
+                if self.board[0][i] == x:
                     self.user_decide = True
                     self.game_board()
                     print(f"The player has won three in a row!")
@@ -135,29 +114,31 @@ class DecideWinner(NoughtsCrosses):
                     self.game_board()
                     print(f"The AI has beaten the player! ")
 
-            if self.board[2][0] == self.board[1][1] == self.board[0][2] != " ":
+        if self.board[0][0] == self.board[1][1] == self.board[2][2] != " ":
 
-                if self.board[2][0] == x:
-                    self.user_decide = True
-                    self.game_board()
-                    print(f"The player has won three in a row!")
+            if self.board[0][0] == x:
+                self.user_decide = True
+                self.game_board()
+                print(f"The player has won three in a row!")
 
-                else:
-                    self.ai_decide = True
-                    self.game_board()
-                    print(f"The AI has beaten the player! ")
+            else:
+                self.ai_decide = True
+                self.game_board()
+                print(f"The AI has beaten the player! ")
+
+        if self.board[2][0] == self.board[1][1] == self.board[0][2] != " ":
+
+            if self.board[2][0] == x:
+                self.user_decide = True
+                self.game_board()
+                print(f"The player has won three in a row!")
+
+            else:
+                self.ai_decide = True
+                self.game_board()
+                print(f"The AI has beaten the player! ")
 
 
 if __name__ == '__main__':
     x_ = NoughtsCrosses()
     x_.main_game()
-
-
-
-
-
-
-
-
-
-
